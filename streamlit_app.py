82a7aea6360817ef38c012cf3330a44675459854
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2025-01-14 17:56:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('We ♥️ Machine Learning')
 
 st.info('Deploy Python ML models with Streamlit')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
