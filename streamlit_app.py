1dba12610ab349b69e0e1b02baec25e040516e6c
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2025-01-14 18:00:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('We ♥️ Machine Learning')
 
 st.info('Deploy Python ML models with Streamlit')
 
-with st.expaner('Data'):
+with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
