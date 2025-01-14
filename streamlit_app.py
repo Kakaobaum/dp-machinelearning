77a01231550136ea7e5434aa3ce8cb2edc23a04a
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2025-01-14 18:17:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,7 @@ with st.expander('Data'):
 #"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
 with st.expander('Visualizations'):
   st.scatter_chart(data=df,x = 'bill_length_mm', y = 'body_mass_g', color = 'species')
+
+# Data preparation
+with st.sidebar:
+    st.header('Input features')
