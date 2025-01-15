01ad26f2274cca7411a4073b8bcba10e2345cfbb
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2025-01-15 15:18:20 +0100

    Update class_exercise.py

--- class_exercise.py
+++ class_exercise.py
@@ -13,10 +13,10 @@
 #sepal_width min 2.0, max 4.4
 #petal_length min 1.0, max 6.9
 #petal_width min 0.1, max 2.5
-#! don't forget to add default values between min and max
+#! don't forget to add default values between min and max 
 # add your code here...
 
-
+''' now you can delete this line
     input_data = {
         'Sepal.Length': sepal_length,
         'Sepal.Width': sepal_width,
@@ -26,26 +26,38 @@
     input_df = pd.DataFrame(input_data, index=[0])
 
 # Data preparation
+
 X = df.drop('Species', axis=1)  # Correct column name
 Y = df['Species']  # Correct column name
 target_mapping = {'setosa': 0, 'versicolor': 1, 'virginica': 2}
 Y_encoded = Y.map(target_mapping)
 
+
 # Train model
+
 clf = RandomForestClassifier()
 clf.fit(X, Y_encoded)
 
+
 # Predict
+
 prediction = clf.predict(input_df)
 prediction_proba = clf.predict_proba(input_df)
 
+
 # Display results
+
 st.write('**Prediction Probabilities**')
 prob_df = pd.DataFrame(prediction_proba, columns=['Setosa', 'Versicolor', 'Virginica'])
 st.write(prob_df)
+''' '''now you can delete this line '''
+
+
+#! don't forget to delete the triple-quoted strings
+
 
 # TODO: Display the predicted species
-#species_mapping = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+# species_mapping = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
 # add code here...
 
 
